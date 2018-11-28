83ac936ca30f000a268ff3aa817a336c792fc65d
Kelli Landry <kellilandry@Kellis-Mac-mini.local>
2018-11-28 17:49:01 -0600

    First save on Topic unit tests

---
+++ spec/unit/topic_spec.js
@@ -0,0 +1,88 @@
+const sequelize = require("../../src/db/models/index").sequelize;
+const Topic = require("../../src/db/models").Topic;
+const Post = require("../../src/db/models").Post;
+
+describe("Topic", () => {
+    beforeEach((done) => {
+        this.topic;
+        this.post;
+        sequelize.sync({force: true}).then((res) => {
+            Topic.create({
+                title: "Coding languages",
+                description: "Languages in Javascript"
+            })
+            .then((topic) => {
+                this.topic = topic;
+                Post.create({
+                    title:"Javascript",
+                    body: "Front end web dev",
+                    topicId: this.topic.id
+                })
+                .then((post) => {
+                    this.post = post;
+                    done();
+                });
+            })
+            .catch((err) => {
+                console.log(err);
+                done();
+            });
+        });
+    });
+    describe("#create()", () => {
+        it("should create a topic with a title and description", (done) => {
+            Topic.create({
+                title: "Node JS",
+                description: "Thread for NodeJS discussion"
+            })
+            .then((topic) => {
+                expect(topic.title).toBe("Node JS");
+                expect(topic.description).toBe("Thread for NodeJS discussion");
+                done();
+            })
+            .catch((err) => {
+                console.log(err);
+                done();
+            });
+        });
+        it("should not create a topic with missing title or description", (done) => {
+            Topic.create({
+                title: "Node JS"
+            })
+            .then((topic) => {
+                done();
+            })
+            .catch((err) => {
+                expect(err.message).toContain("Topic.description cannot be null");
+                expect(err.message).toContain("Topic.id cannot be null");
+                done();
+            })
+        });
+    });
+    describe("#setPosts()", () => {
+        it("should associate an array of posts with one assigned topic", (done) => {
+            Post.create({
+                title: "NodeJS Asynchronicity",
+                body: "Posts discussing asycnchronicity within NodeJS",
+                topicId: this.topic.id
+            })
+            .then((newPost) => {
+                expect(this.topic.id).toBe(this.post.topicId);
+                this.topic.setPosts(newPost)
+                .then((topic) => {
+                    expect(topic.id).toBe(newPost.topicId);
+                    done();
+                });
+            });
+        });
+    });
+    describe("#getPosts()", () => {
+        it("should return an array of posts associated with a specific topic", (done) => {
+            this.topic.getPosts()
+            .then((posts) => {
+                expect(posts[0].title);
+                done();
+            });
+        });
+    });
+});
