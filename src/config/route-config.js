b6e6b53abe394af7d642adc31953d2b28e4535df
Kelli Landry <kellilandry@Kellis-Mac-mini.local>
2018-12-15 19:04:14 -0600

    Voting checkpoint complete

--- src/config/route-config.js
+++ src/config/route-config.js
@@ -5,6 +5,7 @@ module.exports = {
         const postRoutes = require("../routes/posts");
         const userRouters = require("../routes/users");
         const commentRoutes = require("../routes/comments");
+        const voteRoutes = require("../routes/votes");
         if(process.env.NODE_ENV === "test"){
             const mockAuth = require("../../spec/support/mock-auth.js");
             mockAuth.fakeIt(app);
@@ -14,5 +15,6 @@ module.exports = {
         app.use(postRoutes);
         app.use(userRouters);
         app.use(commentRoutes);
+        app.use(voteRoutes);
     }
 }
