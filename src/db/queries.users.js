279f31d9ea250ad63e303092d976ab8183276792
Kelli Landry <kellilandry@Kellis-Mac-mini.local>
2018-12-27 12:09:41 -0600

    Public Profile Assignment done

--- src/db/queries.users.js
+++ src/db/queries.users.js
@@ -2,6 +2,7 @@ const User = require("./models").User;
 const Post = require("./models").Post;
 const Comment = require("./models").Comment;
 const bcrypt = require("bcryptjs");
+const Favorite = require("./models").Favorite;
 
 module.exports = {
     createUser(newUser, callback){
@@ -32,7 +33,11 @@ module.exports = {
                     Comment.scope({method: ["lastFiveFor", id]}).all()
                     .then((comments) => {
                         result["comments"] = comments;
-                        callback(null, result);
+                        Favorite.scope({method: ['lastFiveFor', id]}).all()
+                        .then((favorites) => {
+                            result['favorites'] = favorites;
+                            callback(null, result);
+                        })
                     })
                     .catch((err) => {
                         callback(err);
